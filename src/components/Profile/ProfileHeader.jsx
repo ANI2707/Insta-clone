3d1bd9b0c01f0117a6bbd89c2a9cf394273feaba
Aniket Bhosale <108017771+ANI2707@users.noreply.github.com>
2024-01-09 22:52:13 +0530

    Edit User Profile

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -5,17 +5,19 @@ import {
   Flex,
   VStack,
   Button,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import useUserProfileStore from "../../store/userProfileStore";
 import useAuthStore from "../../store/authStore";
+import EditProfile from "./EditProfile";
 
 const ProfileHeader = () => {
   const { userProfile } = useUserProfileStore();
   const authUser=useAuthStore(state=>state.user);
   const visitingOwnProfileAndAuth=authUser && authUser.username===userProfile.username;
   const visitingAnotherProfileAndAuth=authUser && authUser.username !== userProfile.username;
-
+  const {isOpen,onOpen,onClose}=useDisclosure()
 
 
 
@@ -54,6 +56,7 @@ const ProfileHeader = () => {
                 color={"black"}
                 _hover={{ bg: "whiteAlpha.800" }}
                 size={{ base: "xs", md: "sm" }}
+                onClick={onOpen}
               >
                 Edit Profile
               </Button>
@@ -100,6 +103,7 @@ const ProfileHeader = () => {
         </Flex>
         <Text fontSize={"sm"}>{userProfile.bio}</Text>
       </VStack>
+      {isOpen && <EditProfile isOpen={isOpen} onClose={onClose}/>}
     </Flex>
   );
 };
